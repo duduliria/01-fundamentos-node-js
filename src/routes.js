7170cf1d21f5d468982b4b222057c9bb1d23d601
duduliria <duduliria.dev@gmail.com>
2026-01-31 11:25:50 -0300

    Commit: Rotas da aplicação - Route e Query parameters

--- src/routes.js
+++ src/routes.js
@@ -6,7 +6,7 @@ const database = new Database();
 export const routes = [
   {
     method: "GET",
-    patch: "/users",
+    path: "/users",
     handler: (req, res) => {
       const users = database.select("users");
       return res.end(JSON.stringify(users));
@@ -14,7 +14,7 @@ export const routes = [
   },
   {
     method: "POST",
-    patch: "/users",
+    path: "/users",
     handler: (req, res) => {
       const { name, email } = req.body;
       const user = {
@@ -26,6 +26,13 @@ export const routes = [
       database.insert("users", user);
 
       return res.writeHead(201).end();
+    },
+  },
+  {
+    method: "DELETE",
+    path: "/users/",
+    handler: (req, res) => {
+      return res.end();
     },
   },
 ];
--- src/server.js
+++ src/server.js
@@ -3,6 +3,11 @@ import http from "node:http";
 import { json } from "./middlewares/json.js";
 import { routes } from "./routes.js";
 
+// Query parameters => URL Statefull => filtros, paginacao, nao-obrigatorios
+// Route paarameters => Identificar recurso
+// Request body => Envio de informacoes de um formulario (HTTPs)
+
+
 const sever = http.createServer(async (req, res) => {
   const { method, url } = req;
   await json(req, res);
