7a02d7b9794cb3115c868ec3be120830ecb4a731
duduliria <duduliria.dev@gmail.com>
2026-01-31 11:10:56 -0300

    Commit: Rotas da aplicação - Separando rotas da aplicação

---
+++ src/routes.js
@@ -0,0 +1,31 @@
+import { Database } from "./database.js";
+import { randomUUID } from "node:crypto";
+
+const database = new Database();
+
+export const routes = [
+  {
+    method: "GET",
+    patch: "/users",
+    handler: (req, res) => {
+      const users = database.select("users");
+      return res.end(JSON.stringify(users));
+    },
+  },
+  {
+    method: "POST",
+    patch: "/users",
+    handler: (req, res) => {
+      const { name, email } = req.body;
+      const user = {
+        id: randomUUID(),
+        name,
+        email,
+      };
+
+      database.insert("users", user);
+
+      return res.writeHead(201).end();
+    },
+  },
+];
